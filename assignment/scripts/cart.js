02d1a47ff921fb170381e7384935fa198f47d21c
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-13 12:17:58 -0500

    Got started.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,13 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
-
+function addItem(item){
+    console.log('in function addItem');
+    basket.push(item);
+    return true;
+}
 
 
 
